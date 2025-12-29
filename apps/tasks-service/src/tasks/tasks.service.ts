83800cdb96794f3c14cc2f21f77de86e27adff18
LucasMagnun <120067757+LucasMagnun@users.noreply.github.com>
2025-12-28 23:45:54 -0300

    rabbitmq config and publish event by task creation

--- apps/tasks-service/src/tasks/tasks.service.ts
+++ apps/tasks-service/src/tasks/tasks.service.ts
@@ -1,23 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './entities/task.entity';
 import { Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class TasksService {
   constructor(
+    @Inject('RABBITMQ_SERVICE')
+    private readonly client: ClientProxy,
     @InjectRepository(Task)
     private taskRepo: Repository<Task>,
   ) {}
 
-  create(userId: string, dto: CreateTaskDto) {
+  async create(userId: string, dto: CreateTaskDto) {
     const task = this.taskRepo.create({
       ...dto,
       createdBy: userId,
     });
 
-    return this.taskRepo.save(task);
+    await this.taskRepo.save(task);
+
+    this.client.emit('task.created', {
+      taskId: task.id,
+      userId,
+      title: task.title,
+    });
+
+    return task;
   }
 
   findAll() {
